ec81ed46c227f89983a3ada4ff093b2b84017c91
Aws-Shraideh <amalshraiedeh21@cit.just.edu.jo>
2024-08-14 13:47:20 +0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import Project1Stage1 from './Tasks/Project1Stage1';
 import Project1Stage2 from './Tasks/Project1Stage2';
-import Test from './Tasks/Test';
+
 
 
 
@@ -9,10 +8,7 @@ import Test from './Tasks/Test';
 export default function App()
 {
   return (
-
-      //<Project1Stage1></Project1Stage1>
       <Project1Stage2></Project1Stage2>
-      //<Test></Test>
 
 
   );
